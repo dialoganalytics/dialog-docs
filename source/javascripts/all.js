500b1a1c36eb5515e9ec865826a7b8d8d9c56db1
Philippe Dionne <dionne.phil@gmail.com>
2016-08-18 09:41:09 -0400

    Generate uuids to enable working examples

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
-// This is where it all goes :)
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+}
+
+$(function() {
+  $(".js-distinct-id").each(function(i, el) {
+    el.textContent = uuid();
+  })
+})
